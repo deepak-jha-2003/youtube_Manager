5105f23a8c801ee9d3a157354dd2e9d12489051c
deepak-jha-2003 <deepakjha14052003@gmail.com>
2025-02-24 10:51:49 +0530

    Add YouTube video management functionality with error handling

---
+++ error_handeling.py
@@ -0,0 +1,13 @@
+# basic structure to handel the file
+# file = open("youtube.txt" , 'w')
+
+# try:
+#     file.write('chai and code')
+# finally:
+#     file.close()
+
+
+
+#another way to handel the file
+# with open('youtube.txt' , 'w') as file:
+#     file.write("chai and python")
